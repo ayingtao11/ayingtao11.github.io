aec30df6d5480cd078f29918c612ef4d2d923f70
xin <xinjinweng@gmail.com>
2023-03-04 20:45:01 -0500

    third try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React second try
+          Learn React s try
         </a>
       </header>
     </div>
