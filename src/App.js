cab7587c760843e62281cffafcee8c46700c65e9
xin <xinjinweng@gmail.com>
2023-03-04 19:07:14 -0500

    second try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 跟新
+          Learn React first try
         </a>
       </header>
     </div>
